cd7386e77041522541f4c10d7fba140d6011dd53
Canyen Palmer <Canyen2019@gmail.com>
2025-11-13 10:36:54 -0500

    Add files via upload

---
+++ week_01/our_first_script.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  2 13:05:58 2025
+
+@author: Canye
+"""
+
+2 + 2
+
+3 + 3
+
+5 / 3
+
+5 ** 2
+
+x = 4
+
+y = 5
+
+x ** 3
+
+# components that are created like this are for basic markings
+# A lot of devs use hashtags as basic comments like markdown to use as reminders, descriptions, discussion, etc
+# We can describe what we did and why we did it, as well as report anything intresting
+# Comments are crucial for programming
+
+# There are two types of commenting
+
+"""
+This is multiline commenting so anything in-between will not be evaluated
+
+We can say anything in here and it will not be executed
+"""
+
+### I have seen a lot of people (like our professor) who prefer to use three hashtags
+### They typically leave box comments for large pieces of important information
+
+# Comments tell the machine to not run the line
+
